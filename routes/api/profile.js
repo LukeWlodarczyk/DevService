d392daa75fddf5c39c1ff85a20b6087b5ef3f7c7
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-05-09 00:20:16 +0200

    profile models, route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/test
+// @desc    Tests profile route
+// @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile works' }));
+
+// @route   GET api/profile
+// @desc    Get current user profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+  Profile
+    .findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile)
+    })
+    .catch(err => res.status(404).json(err));
+});
 
 
 module.exports = router;
