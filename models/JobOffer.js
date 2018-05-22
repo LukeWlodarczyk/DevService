deb19723e02f7c0ba0a96cecbc4e2e1b92c2eb2b
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-05-22 23:58:15 +0200

    JobOffer model

---
+++ models/JobOffer.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const JobOffer = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  company: {
+    type: String,
+  },
+  email: {
+    type: String,
+  }
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  position: {
+    type: String,
+  },
+  requirements: {
+    type: [String],
+  },
+  niceToHave: {
+    type: [String],
+  },
+  languages: {
+    type: [String],
+  },
+  canOffer: {
+    type: [String],
+  }
+  description: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
