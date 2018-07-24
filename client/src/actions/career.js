0271657d08e10ff1079da4c50c594d746df57f63
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-24 16:39:38 +0200

    refactor carrer routes and actions

--- client/src/actions/career.js
+++ client/src/actions/career.js
@@ -1,107 +1,125 @@
 import axios from 'axios';
 import {
-  OFFER_LOADING,
-  EDIT_OFFER,
-  ADD_OFFER,
-  DELETE_OFFER,
-  GET_OFFERS,
-  GET_OFFER,
-  GET_ERRORS,
+	OFFER_LOADING,
+	EDIT_OFFER,
+	ADD_OFFER,
+	DELETE_OFFER,
+	GET_OFFERS,
+	GET_OFFER,
+	GET_ERRORS,
 } from '../constants/action-types';
 
 export const getOffer = offerId => dispatch => {
-  dispatch(setOfferLoading());
-  axios
-    .get('/api/career/'+offerId)
-    .then(res =>
-      dispatch({
-        type: GET_OFFER,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_OFFER,
-        payload: err.response.data
-      })
-    );
+	dispatch(setOfferLoading());
+	axios
+		.get('/api/career/' + offerId)
+		.then(res =>
+			dispatch({
+				type: GET_OFFER,
+				payload: res.data,
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_OFFER,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const getOffers = () => dispatch => {
-  dispatch(setOfferLoading());
-  axios
-    .get('/api/career/all')
-    .then(res =>
-      dispatch({
-        type: GET_OFFERS,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_OFFERS,
-        payload: []
-      })
-    );
+	dispatch(setOfferLoading());
+	axios
+		.get('/api/career')
+		.then(res =>
+			dispatch({
+				type: GET_OFFERS,
+				payload: res.data,
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_OFFERS,
+				payload: [],
+			})
+		);
 };
 
 export const addOffer = (offerData, history) => dispatch => {
-  axios
-    .post('/api/career/', offerData)
-    .then(res => {
-      dispatch({
-        type: !offerData.id ? ADD_OFFER : EDIT_OFFER,
-        payload: res.data
-      })
-      history.push('/offer/'+res.data._id);
-    })
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.post('/api/career/', offerData)
+		.then(res => {
+			dispatch({
+				type: ADD_OFFER,
+				payload: res.data,
+			});
+			history.push('/offer/' + res.data._id);
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
+};
+
+export const editOffer = (offerData, history) => dispatch => {
+	axios
+		.put('/api/career/', offerData)
+		.then(res => {
+			dispatch({
+				type: EDIT_OFFER,
+				payload: res.data,
+			});
+			history.push('/offer/' + res.data._id);
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const deleteOffer = (offerId, history) => dispatch => {
-  axios
-    .delete('/api/career/'+offerId)
-    .then(() => {
-      history.push('/offers');
-      dispatch({
-        type: DELETE_OFFER,
-        payload: offerId
-      })
-    })
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.delete('/api/career/' + offerId)
+		.then(() => {
+			history.push('/offers');
+			dispatch({
+				type: DELETE_OFFER,
+				payload: offerId,
+			});
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const sendApplication = ({ offerId, ...data }, history) => dispatch => {
-  axios
-    .post(`/api/career/${offerId}/apply`, data)
-    .then(() => {
-      history.push({
-             pathname:"/success",
-             state:{
-                 message: "Application send!"
-              },
-            });
-    })
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.post(`/api/career/${offerId}/apply`, data)
+		.then(() => {
+			history.push({
+				pathname: '/success',
+				state: {
+					message: 'Application send!',
+				},
+			});
+		})
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const setOfferLoading = () => {
-  return {
-    type: OFFER_LOADING
-  };
+	return {
+		type: OFFER_LOADING,
+	};
 };
--- routes/api/career.js
+++ routes/api/career.js
@@ -12,174 +12,263 @@ const validateOfferInput = require('../../validation/jobOffer');
 const validateApplicationInput = require('../../validation/application');
 const applyTemplate = require('../../services/emailTemplates/applyForAJob');
 
-
 // @route   GET api/career/all
 // @desc    Get all offers
-// @access  Public
-router.get('/all', (req, res) => {
-  const errors = {};
-  JobOffer
-    .find()
-    .sort({ date: -1 })
-    .populate('user', ['name', 'avatar'])
-    .then(offers => {
-      if(!offers || offers.length === 0) {
-        errors.nooffers = 'There are no offers';
-        return res.status(404).send(errors);
-      }
-      res.json(offers);
-    })
-    .catch(err => res.status(404).json({ offers: 'There are no offers' }));
-});
+// @access  Private
+router.get(
+	'/',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		try {
+			const offers = await JobOffer.find()
+				.sort({ date: -1 })
+				.populate('user', ['name', 'avatar']);
+
+			if (!offers || offers.length === 0) {
+				return res
+					.status(404)
+					.json({ error: true, message: 'There are no offers' });
+			}
+
+			res.json(offers);
+		} catch (err) {
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   GET api/career/:id
 // @desc    Get offer by id
-// @access  Public
-router.get('/:id', (req, res) => {
-  JobOffer
-    .findById(req.params.id)
-    .populate('user', ['id', 'username'])
-    .then(offer => res.json(offer))
-    .catch(err => res.status(404).json({ error: true, message: "This offer doesn't exist" }));
-});
+// @access  Private
+router.get(
+	'/:id',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		try {
+			const offer = await JobOffer.findById(req.params.id).populate('user', [
+				'id',
+				'username',
+			]);
+
+			if (!offer) {
+				return res
+					.status(404)
+					.json({ error: true, message: 'Offer with that ID does not exist' });
+			}
+
+			res.json(offer);
+		} catch (err) {
+			if (err.kind === 'ObjectId') {
+				return res
+					.status(404)
+					.json({ error: true, message: 'Offer with that ID does not exist' });
+			}
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   POST api/career
-// @desc    Add or edit job offer
+// @desc    Add job offer
 // @access  Private
 router.post(
-        '/',
-        passport.authenticate('jwt', { session: false }),
-        requireEmailVerification,
-        (req, res) => {
+	'/',
+	passport.authenticate('jwt', { session: false }),
+	requireEmailVerification,
+	async (req, res) => {
+		const { errors, isValid } = validateOfferInput(req.body);
 
-  const { errors, isValid } = validateOfferInput(req.body);
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+		const jobOffer = {};
+		jobOffer.user = req.user.id;
 
-  const jobOffer = {};
-  jobOffer.user = req.user.id;
+		const offerFields = [
+			'company',
+			'website',
+			'location',
+			'email',
+			'phoneNumber',
+			'position',
+			'description',
+		];
 
-  const offerFields = [
-    'company',
-    'website',
-    'location',
-    'email',
-    'phoneNumber',
-    'position',
-    'description',
-  ];
+		offerFields.map(field => {
+			if (req.body[field]) jobOffer[field] = req.body[field];
+		});
 
-  offerFields.map( field => {
-    if(req.body[field]) jobOffer[field] = req.body[field];
-  })
+		const arraySkills = ['requirements', 'niceToHave', 'languages', 'canOffer'];
 
-  const arraySkills = [
-    'requirements',
-    'niceToHave',
-    'languages',
-    'canOffer',
-  ];
+		arraySkills.map(field => {
+			if (typeof req.body[field] !== 'undefined') {
+				jobOffer[field] = req.body[field].split(',').map(item => item.trim());
+			}
+		});
 
-  arraySkills.map( field => {
-    if(typeof req.body[field] !== 'undefined') {
-      jobOffer[field] = req.body[field]
-       .split(",")
-       .map(item => item.trim());
-    }
-  })
+		try {
+			const newOffer = await JobOffer.create(jobOffer);
+			res.json(newOffer);
+		} catch (err) {
+			res.status(400).json(err);
+		}
+	}
+);
 
-  if(req.body.id) {
-    JobOffer
-      .findById(req.body.id)
-      .populate('user', ['id'])
-      .then(offer => {
-        if(req.user.id !== offer.user._id.toString()) {
-           return res.status(401).json({ notauthorized: "User not authorized" });
-        };
+// @route   PUT api/career
+// @desc    Edit job offer
+// @access  Private
+router.put(
+	'/',
+	passport.authenticate('jwt', { session: false }),
+	requireEmailVerification,
+	async (req, res) => {
+		const { errors, isValid } = validateOfferInput(req.body);
 
-        JobOffer
-          .findByIdAndUpdate(req.body.id, { $set: jobOffer }, { new: true })
-          .then(offer => res.json(offer))
-          .catch(err => res.json(err));
-      })
-      return;
-  }
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
 
-  JobOffer
-    .create(jobOffer)
-    .then(offer => res.json(offer))
-    .catch(err => res.json(err))
+		const jobOffer = {};
+		jobOffer.user = req.user.id;
 
-});
+		const offerFields = [
+			'company',
+			'website',
+			'location',
+			'email',
+			'phoneNumber',
+			'position',
+			'description',
+		];
+
+		offerFields.map(field => {
+			if (req.body[field]) jobOffer[field] = req.body[field];
+		});
+
+		const arraySkills = ['requirements', 'niceToHave', 'languages', 'canOffer'];
+
+		arraySkills.map(field => {
+			if (typeof req.body[field] !== 'undefined') {
+				jobOffer[field] = req.body[field].split(',').map(item => item.trim());
+			}
+		});
+
+		try {
+			const offer = await JobOffer.findById(req.body.id).populate('user', [
+				'id',
+			]);
+
+			if (!offer) {
+				res.status(404).json({ error: true, message: '' });
+			}
+
+			if (req.user.id !== offer.user._id.toString()) {
+				return res.status(401).json({
+					error: true,
+					message: 'User not authorized. This offer belongs to different user.',
+				});
+			}
+
+			const updatedOffer = await JobOffer.findByIdAndUpdate(
+				req.body.id,
+				{ $set: jobOffer },
+				{ new: true }
+			);
+
+			res.json(updatedOffer);
+		} catch (e) {
+			if (err.kind === 'ObjectId') {
+				return res
+					.status(404)
+					.json({ error: true, message: 'Offer with that ID does not exist' });
+			}
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   DELETE api/carrer/:id
 // @desc    Remove offer
 // @access  Private
-router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-    JobOffer
-      .findById(req.params.id)
-      .then(offer => {
-        if(!offer) {
-          return res.status(404).json({ offernotexists: 'Offer does not exist' });
-        };
+router.delete(
+	'/:id',
+	passport.authenticate('jwt', { session: false }),
+	requireEmailVerification,
+	async (req, res) => {
+		try {
+			const offer = await JobOffer.findById(req.params.id);
 
-        if(req.user.id !== offer.user.toString()) {
-           return res.status(401).json({ notauthorized: "User not authorized" });
-        };
+			if (!offer) {
+				return res
+					.status(404)
+					.json({ error: true, message: 'Offer with that ID does not exist' });
+			}
 
-        offer
-          .remove()
-          .then( () => res.json({ success: true }))
-          .catch(err => res.json(err));
-      })
-      .catch(err => res.status(404).json({ offernotfound: 'No offer found' }));
-  }
+			if (req.user.id !== offer.user.toString()) {
+				return res.status(401).json({
+					error: true,
+					message: 'User not authorized. This offer belongs to different user.',
+				});
+			}
+
+			await offer.remove();
+
+			res.json({ success: true });
+		} catch (err) {
+			if (err.kind === 'ObjectId') {
+				return res
+					.status(404)
+					.json({ error: true, message: 'Offer with that ID does not exist' });
+			}
+			res.status(400).json(err);
+		}
+	}
 );
 
 // @route   POST api/carrer/apply
 // @desc    Send email to offer author
 // @access  Private
 router.post(
-        '/:id/apply',
-        passport.authenticate('jwt', { session: false }),
-        requireEmailVerification,
-        async (req, res) => {
+	'/:id/apply',
+	passport.authenticate('jwt', { session: false }),
+	requireEmailVerification,
+	async (req, res) => {
+		const { errors, isValid } = validateApplicationInput(req.body);
 
-  const { errors, isValid } = validateApplicationInput(req.body);
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
 
-  if(!isValid) {
-    return res.status(400).json(errors)
-  }
+		try {
+			const offer = await JobOffer.findById(req.params.id);
 
-  const offer = await JobOffer.findById(req.params.id);
+			const emailData = {
+				subject: req.body.subject,
+				recipients: [offer.email],
+				from_email: req.body.email,
+			};
 
-  const emailData = {
-    subject: req.body.subject,
-    recipients: [offer.email],
-    from_email: req.body.email,
-  }
+			if (req.body.attachment) {
+				emailData.attachment = {
+					base64File: req.body.attachment.base64File,
+					fileName: req.body.attachment.fileName,
+				};
+			}
 
-  if(req.body.attachment) {
-    emailData.attachment = {
-      base64File: req.body.attachment.base64File,
-      fileName: req.body.attachment.fileName,
-    }
-  }
+			const mailer = new Mailer(
+				emailData,
+				applyTemplate({ message: req.body.message })
+			);
+			await mailer.send();
 
-  try {
-    const mailer = new Mailer(emailData, applyTemplate({ message: req.body.message }));
-    await mailer.send();
-
-    res.send({ success: true });
-
-  } catch (e) {
-    errors.email = 'Sorry, something went wrong. Try agin later.';
-    return res.status(400).json(errors);
-  }
-
-  }
+			res.send({ success: true });
+		} catch (err) {
+			console.log(err);
+			return res.status(400).json(err);
+		}
+	}
 );
 
 module.exports = router;
