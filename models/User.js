df760731b267479af079b1ea9c282bdd0a2d225c
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-25 00:34:26 +0200

    User model update - hash password when saving / comparePassword

--- models/User.js
+++ models/User.js
@@ -1,35 +1,54 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+	name: {
+		type: String,
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		lowercase: true,
+	},
+	isVerified: {
+		type: Boolean,
+		default: false,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	avatar: {
+		type: String,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 });
+
+UserSchema.pre('save', function(next) {
+	if (!this.isModified('password')) {
+		return next();
+	}
+
+	bcrypt.hash(this.password, 10, (err, hash) => {
+		if (err) return next(err);
+		this.password = hash;
+		next();
+	});
+});
+
+UserSchema.methods.comparePassword = async function comparePassword(
+	candidatePassword
+) {
+	return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = User = mongoose.model('users', UserSchema);
