94c330251c9e3c58928123cb3afb72aae2fbd97d
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-07-24 16:40:40 +0200

    refactor profile routes and actions

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,168 +1,166 @@
 import axios from 'axios';
 import {
-  GET_PROFILE,
-  PROFILE_LOADING,
-  CLEAR_CURRENT_PROFILE,
-  GET_ERRORS,
-  CLEAR_ERRORS,
-  GET_PROFILES,
+	GET_PROFILE,
+	PROFILE_LOADING,
+	CLEAR_CURRENT_PROFILE,
+	GET_ERRORS,
+	CLEAR_ERRORS,
+	GET_PROFILES,
 } from '../constants/action-types';
 import { logoutUser } from './auth';
 
 export const getCurrentProfile = () => dispatch => {
-  dispatch(setProfileLoading());
-  axios
-    .get('/api/profile')
-    .then(res =>
-      dispatch({
-        type: GET_PROFILE,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_PROFILE,
-        payload: {}
-      })
-    );
+	dispatch(setProfileLoading());
+	axios
+		.get('/api/profiles/authenticated')
+		.then(res =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data,
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: {},
+			})
+		);
 };
 
 export const getProfileByUsername = username => dispatch => {
-  dispatch(setProfileLoading());
-  axios
-    .get(`/api/profile/${username}`)
-    .then(res =>
-      dispatch({
-        type: GET_PROFILE,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_PROFILE,
-        payload: err.response.data
-      })
-    );
+	dispatch(setProfileLoading());
+	axios
+		.get(`/api/profiles/${username}`)
+		.then(res =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data,
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const editProfile = (profileData, history) => dispatch => {
-  axios
-    .post('/api/profile', profileData)
-    .then(res => history.push('/dashboard'))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.put('/api/profiles/authenticated', profileData)
+		.then(res => history.push('/dashboard'))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const getProfiles = () => dispatch => {
-  dispatch(setProfileLoading());
-  axios
-    .get('/api/profile/all')
-    .then(res =>
-      dispatch({
-        type: GET_PROFILES,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_PROFILES,
-        payload: []
-      })
-    );
+	dispatch(setProfileLoading());
+	axios
+		.get('/api/profiles')
+		.then(res =>
+			dispatch({
+				type: GET_PROFILES,
+				payload: res.data,
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_PROFILES,
+				payload: [],
+			})
+		);
 };
 
 export const addExperience = (expData, history) => dispatch => {
-  axios
-    .post('/api/profile/experience', expData)
-    .then(res => history.push('/dashboard'))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.post('/api/profiles/authenticated/experience', expData)
+		.then(res => history.push('/dashboard'))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const deleteExperience = id => dispatch => {
-  axios
-    .delete(`/api/profile/experience/${id}`)
-    .then(res =>
-      dispatch({
-        type: GET_PROFILE,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.delete(`/api/profiles/authenticated/experience/${id}`)
+		.then(res =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data,
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const addEducation = (eduData, history) => dispatch => {
-  axios
-    .post('/api/profile/education', eduData)
-    .then(res => history.push('/dashboard'))
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.post('/api/profiles/authenticated/education', eduData)
+		.then(res => history.push('/dashboard'))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const deleteEducation = id => dispatch => {
-  axios
-    .delete(`/api/profile/education/${id}`)
-    .then(res =>
-      dispatch({
-        type: GET_PROFILE,
-        payload: res.data
-      })
-    )
-    .catch(err =>
-      dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-      })
-    );
+	axios
+		.delete(`/api/profiles/authenticated/education/${id}`)
+		.then(res =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data,
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data,
+			})
+		);
 };
 
 export const deleteAccount = () => dispatch => {
-  if (window.confirm('Are you sure? This operation is permanent!')) {
-    axios
-      .delete('/api/profile')
-      .then(res =>
-        dispatch(logoutUser())
-      )
-      .catch(err =>
-        dispatch({
-          type: GET_ERRORS,
-          payload: err.response.data
-        })
-      );
-  }
+	if (window.confirm('Are you sure? This operation is permanent!')) {
+		axios
+			.delete('/api/profiles/authenticated')
+			.then(res => dispatch(logoutUser()))
+			.catch(err =>
+				dispatch({
+					type: GET_ERRORS,
+					payload: err.response.data,
+				})
+			);
+	}
 };
 
 export const setProfileLoading = () => {
-  return {
-    type: PROFILE_LOADING
-  };
+	return {
+		type: PROFILE_LOADING,
+	};
 };
 
 export const clearCurrentProfile = () => {
-  return {
-    type: CLEAR_CURRENT_PROFILE
-  };
+	return {
+		type: CLEAR_CURRENT_PROFILE,
+	};
 };
 
 export const clearErrors = () => {
-  return {
-    type: CLEAR_ERRORS
-  };
+	return {
+		type: CLEAR_ERRORS,
+	};
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -15,252 +15,297 @@ const User = require('../../models/User');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile works' }));
 
-// @route   GET api/profile
+// @route   GET api/profile/authenticated
 // @desc    Get current user profile
 // @access  Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  const errors = {};
-  Profile
-    .findOne({ user: req.user.id })
-    .populate('user', ['name', 'avatar'])
-    .then(profile => {
-      if(!profile) {
-        errors.noprofile = 'There is no profile for this user';
-        return res.status(404).json(errors);
-      }
-      res.json(profile)
-    })
-    .catch(err => res.status(404).json(err));
-});
+router.get(
+	'/authenticated',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		try {
+			const profile = await Profile.findOne({ user: req.user.id }).populate(
+				'user',
+				['name', 'avatar']
+			);
+
+			if (!profile) {
+				return res.status(404).json({
+					error: true,
+					message: 'There is no profile for authenticated user',
+				});
+			}
+			res.json(profile);
+		} catch (err) {
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   GET api/profile/all
 // @desc    Get all profiles
 // @access  Public
-router.get('/all', (req, res) => {
-  const errors = {};
-  Profile
-    .find()
-    .populate('user', ['name', 'avatar'])
-    .then(profiles => {
-      if(!profiles || profiles.length === 0) {
-        errors.noprofile = 'There are no profiles';
-        return res.status(404).send(errors);
-      }
-      res.json(profiles);
-    })
-    .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+
+		if (!profiles || profiles.length === 0) {
+			return res
+				.status(404)
+				.send({ error: true, message: 'There are no profiles' });
+		}
+		res.json(profiles);
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 // @route   GET api/profile/:username
 // @desc    Get profile by username
 // @access  Public
-router.get('/:username', (req, res) => {
-  const errors = {};
-  Profile
-    .findOne({ username: req.params.username })
-    .populate('user', ['name', 'avatar', 'username'])
-    .then(profile => {
-      if(!profile) {
-        return res.status(404).json({ error: true, message: "This user doesn't exist" })
-      }
-      res.json(profile);
-    })
-    .catch(err => res.status(404).json(err))
+router.get('/:username', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			username: req.params.username,
+		}).populate('user', ['name', 'avatar', 'username']);
+
+		if (!profile) {
+			return res.status(404).json({
+				error: true,
+				message: 'Profile with that username does not exist',
+			});
+		}
+		res.json(profile);
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 // @route   GET api/profile/user/:user_id
 // @desc    Get profile by user ID
 // @access  Public
-router.get('/user/:user_id', (req, res) => {
-  const errors = {};
-  Profile
-    .findOne({ user: req.params.user_id })
-    .populate('user', ['name', 'avatar'])
-    .then(profile => {
-      if(!profile) {
-        errors.noprofile = 'There is no profile for this user';
-        return res.status(404).send(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => res.status(404).json({ profile: 'There is no profile for this user' }));
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate('user', ['name', 'avatar']);
+
+		if (!profile) {
+			return res.status(404).send({
+				error: true,
+				message: 'Profile with that user id does not exist',
+			});
+		}
+
+		res.json(profile);
+	} catch (err) {
+		res.stataus(400).json(err);
+	}
 });
 
-// @route   POST api/profile
+// @route   PUT api/profile
 // @desc    Edit user profile
 // @access  Private
-router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.put(
+	'/authenticated',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		const { errors, isValid } = validateProfileInput(req.body);
 
-  const { errors, isValid } = validateProfileInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const profileFields = {};
-  profileFields.user = req.user.id;
-  if (req.body.company) profileFields.company = req.body.company;
-  if (req.body.website) profileFields.website = req.body.website;
-  if (req.body.location) profileFields.location = req.body.location;
-  if (req.body.bio) profileFields.bio = req.body.bio;
-  if (req.body.status) profileFields.status = req.body.status;
-  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
-
-  if (typeof req.body.skills !== 'undefined') {
-    profileFields.skills = req.body.skills
-     .split(",")
-     .map(item => item.trim());
-  }
-
-  profileFields.social = {};
-
-	const socials = ['youtube', 'twitter', 'facebook', 'linkedin', 'instagram'];
-
-	socials.forEach( social => {
-		if(req.body[social]) {
-			if(req.body[social].includes('https://')) {
-				profileFields.social[social] = req.body[social];
-				return;
-			}
-			profileFields.social[social] = 'https://' + req.body[social];
+		if (!isValid) {
+			return res.status(400).json(errors);
 		}
-	})
 
+		const profileFields = {};
+		profileFields.user = req.user.id;
 
+		const fields = [
+			'company',
+			'website',
+			'location',
+			'bio',
+			'status',
+			'githubusername',
+		];
 
-  Profile
-    .findOne({ username: req.user.username })
-    .then(profile => {
-      profile
-        .update({ $set: profileFields }, { new: true })
-        .then(profile => res.json(profile))
-        .catch(err => res.json(err));
-    })
-    .catch( err => res.json(err));
+		fields.forEach(field => {
+			if (req.body[field]) profileFields[field] = req.body[field];
+		});
 
-});
+		!profileFields.website.includes('https://') &&
+			(profileFields.website = 'https://' + profileFields.website);
+
+		if (typeof req.body.skills !== 'undefined') {
+			profileFields.skills = req.body.skills
+				.split(',')
+				.map(item => item.trim());
+		}
+
+		profileFields.social = {};
+
+		const socials = ['youtube', 'twitter', 'facebook', 'linkedin', 'instagram'];
+
+		socials.forEach(social => {
+			if (req.body[social]) {
+				if (req.body[social].includes('https://')) {
+					profileFields.social[social] = req.body[social];
+					return;
+				}
+				profileFields.social[social] = 'https://' + req.body[social];
+			}
+		});
+
+		try {
+			const profile = await Profile.findOneAndUpdate(
+				{
+					username: req.user.username,
+				},
+				{ $set: profileFields },
+				{ new: true }
+			);
+			res.json(profile);
+		} catch (err) {
+			res.status.json(err);
+		}
+	}
+);
 
 // @route   POST api/profile/experience
 // @desc    Add experience to profile
 // @access  Private
-router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.post(
+	'/authenticated/experience',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		const { errors, isValid } = validateExperienceInput(req.body);
 
-  const { errors, isValid } = validateExperienceInput(req.body);
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+		const newExp = {
+			title: req.body.title,
+			company: req.body.company,
+			location: req.body.location,
+			from: req.body.from,
+			to: req.body.to,
+			current: req.body.current,
+			description: req.body.description,
+		};
 
-  Profile
-    .findOne({ user: req.user.id })
-    .then(profile => {
-      const newExp = {
-        title: req.body.title,
-        company: req.body.company,
-        location: req.body.location,
-        from: req.body.from,
-        to: req.body.to,
-        current: req.body.current,
-        description: req.body.description,
-      };
-
-      profile.experience.unshift(newExp);
-
-      profile
-        .save()
-        .then(profile => res.json(profile))
-        .catch(err => res.json(err));
-    })
-    .catch(err => res.status(404).json(err));
-});
+		try {
+			const profile = await Profile.findOneAndUpdate(
+				{ user: req.user.id },
+				{
+					$push: {
+						experience: newExp,
+					},
+				},
+				{ new: true }
+			);
+			res.json(profile);
+		} catch (err) {
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   POST api/profile/education
 // @desc    Add education to profile
 // @access  Private
-router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.post(
+	'/authenticated/education',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		const { errors, isValid } = validateEducationInput(req.body);
 
-  const { errors, isValid } = validateEducationInput(req.body);
+		if (!isValid) {
+			return res.status(400).json(errors);
+		}
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+		const newEdu = {
+			school: req.body.school,
+			degree: req.body.degree,
+			fieldofstudy: req.body.fieldofstudy,
+			from: req.body.from,
+			to: req.body.to,
+			current: req.body.current,
+			description: req.body.description,
+		};
 
-  Profile
-    .findOne({ user: req.user.id })
-    .then(profile => {
-      const newEdu = {
-        school: req.body.school,
-        degree: req.body.degree,
-        fieldofstudy: req.body.fieldofstudy,
-        from: req.body.from,
-        to: req.body.to,
-        current: req.body.current,
-        description: req.body.description,
-      };
+		try {
+			const profile = await Profile.findOneAndUpdate(
+				{ user: req.user.id },
+				{
+					$push: {
+						education: newEdu,
+					},
+				},
+				{ new: true }
+			);
 
-      profile.education.unshift(newEdu);
-
-      profile
-        .save()
-        .then(profile => res.json(profile))
-        .catch(err => res.json(err));
-    })
-    .catch(err => res.status(404).json(err));
-});
+			res.json(profile);
+		} catch (err) {
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   DELETE api/profile/experience/:exp_id
 // @desc    Delete experience from profile
 // @access  Private
-router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Profile
-    .findOne({ user: req.user.id })
-    .then(profile => {
-
-      profile.experience.remove({ _id: req.params.exp_id });
-
-      profile
-        .save()
-        .then(result => res.json(profile))
-        .catch(err => res.json(err));
-
-    })
-    .catch(err => res.status(404).json(err));
-});
+router.delete(
+	'/authenticated/experience/:exp_id',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		try {
+			const profile = await Profile.findOneAndUpdate(
+				{ user: req.user.id },
+				{ $pull: { experience: { _id: req.params.exp_id } } },
+				{ new: true }
+			);
+			res.json(profile);
+		} catch (err) {
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   DELETE api/profile/education/:exp_id
 // @desc    Delete education from profile
 // @access  Private
-router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Profile
-    .findOne({ user: req.user.id })
-    .then(profile => {
-
-      profile.education.remove({ _id: req.params.edu_id });
-
-      profile
-        .save()
-        .then(profile => res.json(profile))
-        .catch(err => res.json(err));
-    })
-    .catch(err => res.status(404).json(err));
-});
+router.delete(
+	'/authenticated/education/:edu_id',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		try {
+			const profile = await Profile.findOneAndUpdate(
+				{ user: req.user.id },
+				{ $pull: { education: { _id: req.params.edu_id } } },
+				{ new: true }
+			);
+			res.json(profile);
+		} catch (err) {
+			res.status(400).json(err);
+		}
+	}
+);
 
 // @route   DELETE api/profile
 // @desc    Delete user and profile
 // @access  Private
-router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Profile
-    .findOneAndRemove({ user: req.user.id })
-    .then(profile => {
-      User
-        .findOneAndRemove({ _id: req.user.id })
-        .then(() => res.json({ succes: true }))
-        .catch(err => res.status(404).json(err));
+router.delete(
+	'/authenticated',
+	passport.authenticate('jwt', { session: false }),
+	async (req, res) => {
+		try {
+			await Profile.findOneAndRemove({ user: req.user.id });
+			await User.findOneAndRemove({ _id: req.user.id });
 
-    })
-    .catch(err => res.status(404).json(err));
-});
-
-
+			res.json({ succes: true });
+		} catch (e) {
+			res.status(400).json(err);
+		}
+	}
+);
 
 module.exports = router;
