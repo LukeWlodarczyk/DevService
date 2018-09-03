f52b689e445968449de456d5f5ae873f7e6a1842
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-09-03 02:09:00 +0200

    center spinner

--- client/src/components/common/Spinner.js
+++ client/src/components/common/Spinner.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
+
 const Spinner = () => (
-  <div className="row" >
-    <div className="col" > </div>
-    <div className="col" >
-      <i className="fas fa-sync fa-spin fa-2x" />
-    </div>
-    <div className="col"></div>
-  </div>
+	<div className="row">
+		<div className="col"> </div>
+		<div style={{ textAlign: 'center' }} className="col">
+			<i className="fas fa-sync fa-spin fa-2x" />
+		</div>
+		<div className="col" />
+	</div>
 );
+
 export default Spinner;
